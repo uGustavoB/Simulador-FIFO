b9279a24684b0369af6b7d33233ca5c45a65221e
Gustavo <gugagabriel56@gmail.com>
2024-09-17 22:07:52 -0300

    Implementando input dos usuários

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from fifoSimulador import FIFO
 
 def main():
     fifo = FIFO()
-    fifo.adicionar_processo('P1', 0, 3)
-    fifo.adicionar_processo('P2', 2, 6)
-    fifo.adicionar_processo('P3', 4, 4)
+    # Adicionando processos manualmente
+    numero_processos = int(input("Digite o número de processos: "))
+    for i in range(numero_processos):
+        nome = input(f"Digite o nome do processo {i+1}: ")
+        tempo_chegada = int(input(f"Digite o tempo de chegada do processo {i+1}: "))
+        duracao = int(input(f"Digite a duração do processo {i+1}: "))
+        fifo.adicionar_processo(nome, tempo_chegada, duracao)
 
     print("\nAntes da execução:")
     print(fifo.fila)
