7c590b7e627d2e7505fcaad27bd3775597652985
Gustavo <gugagabriel56@gmail.com>
2024-09-17 21:38:37 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,10 @@
+from Estruturas.FilaSequencial import Fila
+
+'''
+Algorítmo principal escolhido: FIFO(Fila)
+
+Objetivo:
+- Criar um sistema de gerenciamento de processos que simule o funcionamento de um sistema operacional.
+- O sistema deve ser capaz de criar, executar e finalizar processos.
+- O sistema deve ser capaz de gerenciar a fila de processos de acordo com a ordem de chegada.
+'''
